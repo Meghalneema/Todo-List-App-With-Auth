616b77d9ebbd9fd846bb5c9e7a05ca86eb0c0368
Meghal Neema <115687746+Meghalneema@users.noreply.github.com>
2024-01-16 18:51:00 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  headers: [
+    {
+      key: "Cross-Origin-Embedder-Policy",
+      value: "unsafe-none",
+    }
+  ]
 })
